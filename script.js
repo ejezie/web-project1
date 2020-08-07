b5234b93d32561b027ee2929865076b9c7fc7f07
Chinedu Ejezie <shady07chinex@gmail.com>
2020-08-07 09:23:58 -0700

    fifth commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".form-wrapper")
+
+document.querySelector(".order-button").addEventListener('click', ()=>{
+    container.classList.add('change');
+})
+
+document.querySelector(".cancel-button").addEventListener('click', ()=>{
+    container.classList.remove('change');
+})
