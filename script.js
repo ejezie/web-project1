8798cb59f57a085f71275226b2d0264ec5f61083
EJEZIE CHINEDU EMMANUEL <54438361+ejezie@users.noreply.github.com>
2021-07-02 20:31:03 +0100

    Update script

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const container = document.querySelector(".form-wrapper")
 document.querySelector(".order-button").addEventListener('click', ()=>{
     container.classList.add('change');
 })
-
 document.querySelector(".cancel-button").addEventListener('click', ()=>{
     container.classList.remove('change');
+})
+document.querySelector(".cancel-button").addEventListener('click', ()=>{
+    container.classList.add('value');
 })
